21860e45ed7e9ce06d58647a5eeebba33db31691
Phungxuamduc2002 <phungxuanduc2002@gmail.com>
2024-06-21 18:07:30 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PXDBaiThi_23_24.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
